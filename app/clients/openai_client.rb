d9187572be66beab36e649591ea469c4d5743bbd
leosantosw <leonardosantosx0077@gmail.com>
2024-01-06 21:55:31 -0300

    refactor: openai client

--- app/clients/openai_client.rb
+++ app/clients/openai_client.rb
@@ -3,16 +3,17 @@ require 'dotenv/load'
 
 class OpenaiClient
     def initialize
-        @client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
+        @openai_client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
     end
 
-    def call 
-        response = @client.chat(
+    def call(prompt)
+        response = @openai_client.chat(
             parameters: {
                 model: "gpt-3.5-turbo",
-                messages: [{ role: "user", content: "Me fala algumas frases em inglês?"}],
+                messages: [{ role: "user", content: prompt }],
                 temperature: 0.7,
             })
+        puts response
         
         response.dig('choices', 0, 'message', 'content')
     end
